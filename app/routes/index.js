1915594f27b61d0bef0c3b2d1d4ee4b8d5340517
dongchao612 <dongchao980612@163.com>
2024-08-02 23:27:25 +0800

    change root to /

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/hello', function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.json({ message: 'Hello Node.js!' });
 });
 
