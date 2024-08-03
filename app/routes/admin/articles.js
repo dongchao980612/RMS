615801a2e354bee19a77a399d89dcefc3ef22c75
dongchao612 <dongchao980612@163.com>
2024-08-03 10:41:45 +0800

    add CURD

--- app/routes/admin/articles.js
+++ app/routes/admin/articles.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const { Article } = require('../../models');
 
-/* GET home page. */
+/**
+ * 查询文章列表
+ * GET /admin/articles
+ */
 router.get('/', async function (req, res, next) {
     try {
         const condition = {
@@ -28,7 +31,10 @@ router.get('/', async function (req, res, next) {
 });
 
 
-/* GET home page. */
+/**
+ * 查询文章详情
+ * GET /admin/articles/:id
+ */
 router.get('/:id', async function (req, res, next) {
     try {
         const { id } = req.params;
@@ -51,6 +57,90 @@ router.get('/:id', async function (req, res, next) {
             code: false,
             data: [],
             message: '文章列表获取失败',
+            errors: [error.message]
+        });
+    }
+});
+
+/**
+ * 创建文章
+ * POST /admin/articles
+ */
+router.post('/', async function (req, res, next) {
+    try {
+        const article = await Article.create(req.body);
+        res.status(201).json({
+            code: true,
+            data: article,
+            message: '文章创建成功'
+        });
+    } catch (error) {
+        res.status(500).json({
+            code: false,
+            data: [],
+            message: '文章创建失败',
+            errors: [error.message]
+        });
+    }
+});
+/**
+ * 删除文章
+ * DELETE /admin/articles/:id
+ */
+router.delete('/:id', async function (req, res, next) {
+    try {
+        const { id } = req.params;
+        const article = await Article.findByPk(id);
+        if (article) {
+            await article.destroy();
+            res.json({
+                code: true,
+                message: '文章删除成功'
+            });
+
+        } else {
+            return res.status(404).json({
+                code: false,
+                message: '文章不存在'
+            });
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            code: false,
+            data: [],
+            message: '文章删除失败',
+            errors: [error.message]
+        });
+    }
+});
+
+/*
+ * 更新文章
+ * PUT /admin/articles/:id
+ */
+router.put('/:id', async function (req, res, next) {
+    try {
+        const { id } = req.params;
+        const article = await Article.findByPk(id);
+        if (article) {
+            await article.update(req.body);
+            res.json({
+                code: true,
+                data: article,
+                message: '文章更新成功'
+            });
+        } else {
+            return res.status(404).json({
+                code: false,
+                message: '文章不存在'
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            code: false,
+            data: [],
+            message: '文章更新失败',
             errors: [error.message]
         });
     }
