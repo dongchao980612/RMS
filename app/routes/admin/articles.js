e6c3ff2a948af4a79810a0b8a5fa117b100ea048
dongchao612 <dongchao980612@163.com>
2024-08-03 11:37:39 +0800

    add fuzzy search and pagination query

--- app/routes/admin/articles.js
+++ app/routes/admin/articles.js
@@ -2,27 +2,49 @@ const express = require('express');
 const router = express.Router();
 
 const { Article } = require('../../models');
-
+const { Op } = require('sequelize');
 /**
  * 查询文章列表
  * GET /admin/articles
  */
 router.get('/', async function (req, res, next) {
     try {
+        const query = req.query;
+
+        const currentPage = Math.abs(parseInt(query.currentPage)) || 1;
+        const pageSize = Math.abs(parseInt(query.pageSize)) || 10;
+
+        const offset = (currentPage - 1) * pageSize;
+
         const condition = {
-            order: [['id', 'DESC']]
+            order: [['id', 'ASC']],
+            limit: pageSize,
+            offset: offset
         };
 
-        const articles = await Article.findAll(condition);
+        if (query.title) {
+            condition.where = {
+                title: {
+                    [Op.like]: `%${query.title}%`
+                }
+            };
+        }
+
+        const { count, rows } = await Article.findAndCountAll(condition);
 
         res.json({
-            code: true,
-            data: articles,
+            status: true,
+            pagination: {
+                total: count,
+                currentPage,
+                pageSize
+            },
+            data: rows,
             message: '文章列表获取成功'
         });
     } catch (error) {
         res.status(500).json({
-            code: false,
+            status: false,
             data: [],
             message: '文章列表获取失败',
             errors: [error.message]
@@ -38,23 +60,22 @@ router.get('/', async function (req, res, next) {
 router.get('/:id', async function (req, res, next) {
     try {
         const { id } = req.params;
-
         const article = await Article.findByPk(id)
 
         if (!article) {
             return res.status(404).json({
-                code: false,
+                status: false,
                 message: '文章不存在'
             });
         }
         res.json({
-            code: true,
+            status: true,
             data: article,
             message: '文章详情获取成功'
         });
     } catch (error) {
         res.status(500).json({
-            code: false,
+            status: false,
             data: [],
             message: '文章列表获取失败',
             errors: [error.message]
@@ -70,13 +91,13 @@ router.post('/', async function (req, res, next) {
     try {
         const article = await Article.create(req.body);
         res.status(201).json({
-            code: true,
+            status: true,
             data: article,
             message: '文章创建成功'
         });
     } catch (error) {
         res.status(500).json({
-            code: false,
+            status: false,
             data: [],
             message: '文章创建失败',
             errors: [error.message]
@@ -94,20 +115,20 @@ router.delete('/:id', async function (req, res, next) {
         if (article) {
             await article.destroy();
             res.json({
-                code: true,
+                status: true,
                 message: '文章删除成功'
             });
 
         } else {
             return res.status(404).json({
-                code: false,
+                status: false,
                 message: '文章不存在'
             });
         }
 
     } catch (error) {
         res.status(500).json({
-            code: false,
+            status: false,
             data: [],
             message: '文章删除失败',
             errors: [error.message]
@@ -126,21 +147,51 @@ router.put('/:id', async function (req, res, next) {
         if (article) {
             await article.update(req.body);
             res.json({
-                code: true,
+                status: true,
                 data: article,
                 message: '文章更新成功'
             });
         } else {
             return res.status(404).json({
-                code: false,
+                status: false,
                 message: '文章不存在'
             });
         }
     } catch (error) {
         res.status(500).json({
-            code: false,
+            status: false,
             data: [],
             message: '文章更新失败',
+            errors: [error.message]
+        });
+    }
+});
+
+/*
+ * 文章搜索
+ * GET /admin/articles/page/:page
+ */
+router.get('/page/:page', async function (req, res, next) {
+    try {
+        const { page } = req.params;
+        const condition = {
+            order: [['id', 'DESC']]
+        };
+        const articles = await Article.findAndCountAll({
+            ...condition,
+            limit: 10,
+            offset: (page - 1) * 10
+        });
+        res.json({
+            status: true,
+            data: articles,
+            message: '文章搜索成功'
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            data: [],
+            message: '文章搜索失败',
             errors: [error.message]
         });
     }
